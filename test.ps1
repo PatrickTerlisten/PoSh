fca602f3dc8f96ac1d9dfac49a8d730235f7cc4c
Patrick Terlisten <patrick@blazilla.de>
2016-11-24 15:10:44 +0100

    First steps
    Start of a new script. Don't know what's the goal

--- test.ps1
+++ test.ps1
@@ -18,3 +18,19 @@ This script is provided "AS IS" with no warranty expressed or implied. Run at yo
 .LINK
 http://www.vcloudnine.de
 #>
+
+### Set Parameters ###
+# HA Isolationaddresses
+
+$cluster = Get-Cluster -Name YourCluster
+$isolationaddress1 = '192.168.100.1'
+$isolationaddress2 = '192.168.100.2'
+
+New-AdvancedSetting -Entity $cluster -Type ClusterHA -Name 'das.isolationaddress1' -Value $isolationaddress1
+New-AdvancedSetting -Entity $cluster -Type ClusterHA -Name 'das.isolationaddress1' -Value $isolationaddress2
+
+New-AdvancedSetting -Entity $cluster -Type ClusterHA -Name 'das.usedefaultisolationaddress' -Value false
+Once these options are set you have to reconfigure HA and the simplest way I found to do this was to disable and re-enable HA.
+Set-Cluster -Cluster $cluster -HAEnabled:$false
+
+Set-Cluster -Cluster $cluster -HAEnabled:$true
