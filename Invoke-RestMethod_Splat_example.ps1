4d2f4cc82895773692098fed5234a3fb6fb1930f
Patrick Terlisten <patrick@blazilla.de>
2016-12-13 17:45:34 +0100

    Moved Invoke-RestMethod_Splat_example.ps1 to master repo

---
+++ Invoke-RestMethod_Splat_example.ps1
@@ -0,0 +1,22 @@
+﻿# Username and Password
+$user = "admin"
+$pass = "password"
+
+# Hashtable that includes data
+$rawbody = @{
+    first='joe'
+    lastname='doe'
+}
+
+# Convert ashtable to JSON format
+$jsonbody = $rawbody | ConvertTo-Json
+
+# Hashtable that includes the parameters for the Invoke-RestMethod call
+$params = @{uri = 'https://192.168.199.17:9443/rest/v1/config/edgeservice/view';
+                   Method = 'Get';
+                   Headers = @{Authorization = 'Basic ' + [System.Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("${user}:${pass}"));}
+                   Body = $jsonbody;
+                   ContentType = 'application/json'
+   }
+
+Invoke-RestMethod @params
