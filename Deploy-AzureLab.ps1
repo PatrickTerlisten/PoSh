5e5e77ac7a68a812bc54e1fbda0354354dc1c4aa
Patrick Terlisten <patrick@blazilla.de>
2017-01-14 17:49:05 +0100

    Removed some files
    Removed some PoSh I don't need anymore

---
+++ Deploy-AzureLab.ps1
@@ -0,0 +1,47 @@
+﻿## Global
+$ResourceGroupName = "LAB-RG1"
+$Location = "WestEurope"
+
+## Storage
+$StorageName = "labrg1storageaccount"
+$StorageType = "Standard_LRS"
+
+## Network
+$InterfaceName = "DC01-NIC1"
+$SubnetName = "Subnet_172_16_0_0"
+$VNetName = "LAB-RG1-NET"
+$VNetAddressPrefix = "172.16.0.0/16"
+$VNetSubnetAddressPrefix = "172.16.0.0/24"
+
+## Compute
+$VMName = "DC01"
+$ComputerName = "DC01"
+$VMSize = "Standard_A1"
+$OSDiskName = $VMName + "OSDisk"
+
+# Resource Group
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+
+# Storage
+$StorageAccount = New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageName -Type $StorageType -Location $Location
+
+# Network
+#$PIp = New-AzureRmPublicIpAddress -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Dynamic
+$SubnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $VNetSubnetAddressPrefix
+$VNet = New-AzureRmVirtualNetwork -Name $VNetName -ResourceGroupName $ResourceGroupName -Location $Location -AddressPrefix $VNetAddressPrefix -Subnet $SubnetConfig
+#$Interface = New-AzureRmNetworkInterface -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[0].Id -PublicIpAddressId $PIp.Id
+$Interface = New-AzureRmNetworkInterface -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[0].Id
+
+# Compute
+
+## Setup local VM object
+$Credential = Get-Credential
+$VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
+$VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Set-AzureRmVMSourceImage -VM $VirtualMachine -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2012-R2-Datacenter -Version "latest"
+$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $Interface.Id
+$OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + "vhds/" + $OSDiskName + ".vhd"
+$VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -CreateOption FromImage
+
+## Create the VM in Azure
+New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine
